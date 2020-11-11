9f86649f9773be145ad8d6eed21c81d88fed98a2
Joni Hasanen <joni@pieceofcode.com>
2020-11-12 00:26:16 +0200

    Set default offset
    Measured with bed springs replaced with nuts

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -994,7 +994,8 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -42, -7, 0 }
+ 
+#define NOZZLE_TO_PROBE_OFFSET { -42, -7, -2.15 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
